01f46307a93286a4d14677add95277cb92462ab0
Alex Linden <alextl2004@gmail.com>
2024-02-20 17:09:39 -0800

    update css

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
-import Image from 'next/image'
+//A pomodoro timer that counts down from 25 minutes
+//The countdown is displayed in the format mm:ss
+//The countdown is updated every second
+//The countdown is stopped when it reaches 00:00
 
 export default function Home() {
   return (
-    <div>Pomo</div>
-  )
+    <>
+      <span className="loading loading-spinner loading-lg">
+      </span>
+      <span className="countdown font-mono text-2xl">
+        <span style={{ "--value": 10 }}>25</span>:
+        <span style={{ "--value": 24 }}>24</span>
+      </span>
+    </>
+  );
 }
